adf1ae697bd322abd43a0d1bd9bddc31d3671112
Nick Newton <nickanewton@gmail.com>
2022-03-29 21:35:37 +0000

    changed click counter to boolean.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,16 +1,15 @@
-var clickCount = 1;
-
+var lightsOn = true;
 var $circle = document.querySelector('.circle');
 var $body = document.querySelector('body');
 
 function handleClickEvent(event) {
-  clickCount++;
-  if (clickCount % 2 === 0) {
-    $circle.className = 'circle gray';
-    $body.className = 'black';
-  } else {
+  lightsOn = !lightsOn;
+  if (lightsOn === true) {
     $circle.className = 'circle';
     $body.className = 'off-yellow';
+  } else if (lightsOn === false) {
+    $circle.className = 'circle gray';
+    $body.className = 'black';
   }
 }
 
