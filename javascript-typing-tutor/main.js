0a996e462e1b9d2ab5ba1cd6fe10b7900cb4b828
Nick Newton <nickanewton@gmail.com>
2022-03-31 22:24:19 +0000

    saving progress almost done.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -4,13 +4,16 @@ var count = -1;
 
 function handleKeyDownEvent(event) {
   count++;
+  var underline = $span[count + 1];
+
   if (event.key === $span[count].textContent) {
     $span[count].className = 'green';
+    if (count < 29) {
+      underline.className = 'underline';
+    }
   } else if (event.key[count] !== $span[count].textContent) {
     $span[count].className = 'red';
     count--;
-  } else {
-    $span.className = 'underline';
   }
 }
 
