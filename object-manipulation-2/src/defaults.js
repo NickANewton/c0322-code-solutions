9ec1950327e4a3383eac3f370c8e31ac53e8940f
Nick Newton <nickanewton@gmail.com>
2022-04-25 02:53:44 +0000

    Completed object-manipulation-2 exercise.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,23 @@
 /* exported defaults */
+/*
+- Create a function that Assigns properties of source object to the target object for all properties that are not defined on target. (no return)
+- this function has 2 parameters (target, source)
+- ex input const target = {
+  foo: 1,
+  baz: 3
+};
+const source = {
+  bar: 2
+};
+
+- example output { foo: 1, bar: 2, baz: 3 }
+- Cycle through each property in source
+- IF prop is NOT = to a property in target, add that property and value to target */
+
+function defaults(target, source) {
+  for (var prop in source) {
+    if (!Object.hasOwn(target, prop)) {
+      target[prop] = source[prop];
+    }
+  }
+}
--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,23 @@
 /* exported invert */
+/*
+- Create a function that returns a new object with the keys and values of the input object swapped.
+- this function has 1 parameter (source)
+- ex input age: NaN, occupation: 'programmer', language: 'JavaScript'
+- ex ouput NaN: 'age', programmer: 'occupation', JavaScript: 'language'
+- create a new object
+- loop through through each key of the input object AND:
+  - set each value equal a new variable
+  - set each key equal to a new variable
+- use bracket notation to pass in the new property ariable and set it = tp the new value variable.
+return the new object.
+*/
+
+function invert(source) {
+  var keyValuesInterted = {};
+  for (var prop in source) {
+    var newProp = source[prop];
+    var newValue = prop;
+    keyValuesInterted[newProp] = newValue;
+  }
+  return keyValuesInterted;
+}
