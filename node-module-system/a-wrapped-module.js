77c5853e77e3752c1098f29577ffbb8713103d82
Nick Newton <nickanewton@gmail.com>
2022-05-03 23:58:38 +0000

    Completed a-wrapped-module.js exercise.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('typeOf exports:', typeof (exports));
+console.log('valueOf exports:', exports);
+
+console.log('typeOf require', typeof (require));
+console.log('valueOf require:', require);
+
+console.log('typeOf module:', typeof (module));
+console.log('valueOf module:', module);
+
+console.log('typeOf __filename:', typeof (__filename));
+console.log('valueOf __filename:', __filename);
+
+console.log('typeOf __dirname:', typeof (__dirname));
+console.log('valueOf __dirname:', __dirname);
